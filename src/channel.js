f7dc278cd049ea4919986535d7d5d83d3119c4db
Kevin <z5420502@ad.unsw.edu.au>
2023-02-20 21:08:45 +1100

    Pushed stub functon channelDetailsV1 into function branch

---
+++ src/channel.js
@@ -0,0 +1,33 @@
+// Sample stub for the channelDetailsV1 function
+// Returns a stub value
+    
+/**
+ * channelDetailsV1
+ * @param {*} authUserId 
+ * @param {*} channelId 
+ * @returns 
+ */
+
+function channelDetailsV1(authUserId, channelId) {
+    return {
+        name: 'Hayden',
+        ownerMembers: [
+            {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+            }
+        ],
+        allMembers: [
+            {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+            }
+        ],
+    }
+}
