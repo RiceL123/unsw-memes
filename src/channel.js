22897bc9655830b26d8b421df55363ca35257b44
Kevin <z5420502@ad.unsw.edu.au>
2023-02-20 21:21:25 +1100

    added channelVoidV1 stub function to branch

--- src/channel.js
+++ src/channel.js
@@ -1,33 +1,15 @@
-// Sample stub for the channelDetailsV1 function
-// Returns a stub value
-    
+// Sample stub for the channelJoinV1 function
+// Returns a blank stub value
+
 /**
- * channelDetailsV1
+ * 
  * @param {*} authUserId 
  * @param {*} channelId 
- * @returns 
+ * @returns {*}
  */
 
-function channelDetailsV1(authUserId, channelId) {
+function channelJoinV1(authUserId, channelId) {
     return {
-        name: 'Hayden',
-        ownerMembers: [
-            {
-            uId: 1,
-            email: 'example@gmail.com',
-            nameFirst: 'Hayden',
-            nameLast: 'Jacobs',
-            handleStr: 'haydenjacobs',
-            }
-        ],
-        allMembers: [
-            {
-            uId: 1,
-            email: 'example@gmail.com',
-            nameFirst: 'Hayden',
-            nameLast: 'Jacobs',
-            handleStr: 'haydenjacobs',
-            }
-        ],
-    }
+
+    };
 }
