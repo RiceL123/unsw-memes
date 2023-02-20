6f27b6e10f2247071963579daf830b4b7f3263f1
Calvin <z5413994@ad.unsw.edu.au>
2023-02-20 21:08:27 +1100

    Added stub channelInviteV1

---
+++ src/channel.js
@@ -0,0 +1,12 @@
+/**
+ * channelInviteV1
+ * @param {*} authUserId 
+ * @param {*} channelId 
+ * @param {*} uId 
+ * @returns {}
+ */
+function channelInviteV1(authUserId, channelId, uId) {
+    return {
+
+    };
+}
