faaac3edd46f5e706a4dff9b1eb4d9b8ec394fec
CatherineKim <z5418928@ad.unsw.edu.au>
2023-02-20 21:08:23 +1100

    Added stub channelsCreateV1

---
+++ src/channels.js
@@ -0,0 +1,13 @@
+/**
+ * channelsCreateV1
+ * @param {*} authUserId 
+ * @param {*} name 
+ * @param {*} isPublic 
+ * @returns { channelId: 1 }
+ */
+function channelsCreateV1(authUserId, name, isPublic) {
+    return {
+        channelId: 1,
+    };
+  }
+  
