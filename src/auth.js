37aa79049126681e7f97d40b5beb5704b32da8c7
PatrickHuang <z5421282@ad.unsw.edu.au>
2023-02-20 21:26:45 +1100

    added parameters

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,11 @@
+/**
+ * authRegisterV1
+ * @param {*} email 
+ * @param {*} password 
+ * @param {*} nameFirst 
+ * @param {*} nameLast 
+ * @returns 
+ */
 function authRegisterV1(email, password, nameFirst, nameLast) {
     return {
         authUserId: 1,
