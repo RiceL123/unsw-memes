84833b00dfb4ad8db8f97d296f11f74f4fc02ef3
Eric Liao <z5422235@ad.unsw.edu.au>
2023-02-21 16:39:03 +1100

    Starter code for channelsListAllV1 to return given stub object

---
+++ src/channels.js
@@ -0,0 +1,12 @@
+// Sample stub for the channelsListAllV1
+// Returns given stub object
+function channelsListAllV1(authUserId) {
+    return {
+        channels: [
+            {
+            channelId: 1,
+            name: 'My Channel',
+            }
+        ], 
+    };
+}
