a2268ea305f97acd9eb8b8ba8434d631d50792cd
Kevin Luong <kevin.luong1@student.unsw.edu.au>
2023-02-21 16:25:45 +1100

    Updating commenting for stub function channelJoinV1-it0

--- src/channel.js
+++ src/channel.js
@@ -1,13 +1,6 @@
 // Sample stub for the channelJoinV1 function
 // Returns a blank stub value
 
-/**
- * 
- * @param {*} authUserId 
- * @param {*} channelId 
- * @returns {*}
- */
-
 function channelJoinV1(authUserId, channelId) {
     return {
 
