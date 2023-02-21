ffc7e182ea0061a9f5f1c392a0c8be5ebf196067
Catherine Kim <catherine.kim4@student.unsw.edu.au>
2023-02-21 16:36:45 +1100

    Updated function comments to new format

--- src/channels.js
+++ src/channels.js
@@ -1,10 +1,6 @@
-/**
- * channelsCreateV1
- * @param {*} authUserId 
- * @param {*} name 
- * @param {*} isPublic 
- * @returns { channelId: 1 }
- */
+// Sample stub for the channelsCreateV1 function
+// Returns given stub value 
+
 function channelsCreateV1(authUserId, name, isPublic) {
     return {
         channelId: 1,
