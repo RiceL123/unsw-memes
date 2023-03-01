efea3362654940cc168596466f21c5e46ab7330c
Eric Liao <z5422235@ad.unsw.edu.au>
2023-03-01 23:32:26 +1100

    basic functionality of generating new id implemented WITHOUT any error checking

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,5 @@
+import { getData, setData } from './dataStore.js';
+
 // Stub function for authLoginV1
 // Returns authUserId
 
@@ -11,8 +13,32 @@ function authLoginV1(email, password) {
 // Returns authUserId
 
 function authRegisterV1(email, password, nameFirst, nameLast) {
+    let data = getData();
+
+    let uId = 1;
+    if (data.users.length > 0) {
+        uId = Math.max.apply(null, data.users.map(x => x.uId)) + 1;
+    }
+
+    // if the newly generated uId already exists, then return error
+    if (data.users.some(x => x.uId === uId)) {
+        return { error: 'could not generate new authUserId' };
+    }
+
+    const newUser = {
+        uId: uId,
+        nameFirst: nameFirst,
+        nameLast: nameLast,
+        email: email,
+        password: password
+    };
+
+    data.users.push(newUser);
+
+    setData(data);
+
     return {
-        authUserId: 1
+        authUserId: uId
     };
 }
 
