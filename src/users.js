8f2b2e46f963035ce5d8ba3e275941f9935d7a1d
PatrickHuang <z5421282@ad.unsw.edu.au>
2023-03-05 21:27:14 +1100

    started implementation of userProfileV1

--- src/users.js
+++ src/users.js
@@ -1,10 +1,22 @@
+import validator from 'validator';
+import { getData, setData } from './dataStore.js';
+import { authLoginV1, authRegisterV1 } from './auth.js';
+
 function userProfileV1(authUserId, uId) {
+  let data = getData();
+
+  if (!(data.users.some(x => x.uId === uId))) {
+    return { error: 'could not generate new authUserId' };
+  };
+
+
   return {
     uId: 1,
     nameFirst: 'Hayden',
     nameLast: 'Jacobs',
     email: 'example@gmail.com',
     handleStr: 'haydenjacobs',
-    password: 'password',
   }
 }
+
+export { userProfileV1 };
