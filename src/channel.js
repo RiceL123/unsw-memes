606afa18f2487f4abb6d8689fc49b0011dd3d8ca
z5418928 <z5418928@vx08>
2023-03-09 02:03:26 +1100

    Implementing channelsCreateV1

--- src/channel.js
+++ src/channel.js
@@ -1,28 +1,66 @@
-// Sample stub for the channelDetailsV1 function
-// Returns sample stub data.
-    
+import { getData, setData } from './dataStore.js';
+
+/**
+ * 
+ * @param {number} authUserId 
+ * @param {number} channelId 
+ * @returns {{name, isPublic, ownerMembers, allMembers}}
+ */
 function channelDetailsV1(authUserId, channelId) {
-    return {
-        name: 'Hayden',
-        ownerMembers: [
-            {
-            uId: 1,
-            email: 'example@gmail.com',
-            nameFirst: 'Hayden',
-            nameLast: 'Jacobs',
-            handleStr: 'haydenjacobs',
-            }
-        ],
-        allMembers: [
-            {
-            uId: 1,
-            email: 'example@gmail.com',
-            nameFirst: 'Hayden',
-            nameLast: 'Jacobs',
-            handleStr: 'haydenjacobs',
-            }
-        ],
-    };
+  const data = getData();
+
+  if (!(data.users.some(x => x.uId === authUserId))) {
+    return { error: 'authUserId not found' };
+  }
+
+  const channelObj = data.channels.find(x => x.channelId === channelId);
+
+  if (channelObj === undefined) {
+    return { error: 'channelId not found' };
+  }
+
+  // even if the user is a global owner, they still need to be a member
+  if (!(channelObj.allMembersIds.includes(authUserId))) {
+    return { error: 'authUserId is not a member of the channel' };
+  }
+
+  let allMembers = [];
+  let ownerMembers = [];
+
+  // loop through all members (owner members is a subset of all members)
+  for (const userId of channelObj.allMembersIds) {
+    // find the corresponding user in the channel ownerMembers array
+    const userObj = data.users.find(x => x.uId === userId);
+
+    // add the relevant details to allMembers array
+    if (userObj !== undefined) {
+      allMembers.push({
+        uId: userObj.uId,
+        email: userObj.email,
+        nameFirst: userObj.nameFirst,
+        nameLast: userObj.nameLast,
+        handleStr: userObj.handleStr,
+      });
+    }
+
+    // add the relevant details to ownerMembers array
+    if (userObj !== undefined && channelObj.ownerMembersIds.includes(userObj.authUserId)) {
+      ownerMembers.push({
+        uId: userObj.uId,
+        email: userObj.email,
+        nameFirst: userObj.nameFirst,
+        nameLast: userObj.nameLast,
+        handleStr: userObj.handleStr,
+      });
+    }
+  }
+
+  return {
+    name: channelObj.name,
+    isPublic: channelObj.isPublic,
+    ownerMembers: ownerMembers,
+    allMembers: allMembers,
+  }
 }
 
 // Sample stub for the channelJoinV1 function
@@ -60,3 +98,6 @@ function channelMessagesV1(authUserId, channelId, start) {
           end: 50,
     };
 }
+
+
+export { channelDetailsV1 };
--- src/channels.test.js
+++ src/channels.test.js
@@ -9,7 +9,7 @@ beforeEach(() => {
     clearV1();
 });
 
-describe('ChannelsCreateV1', () =>{
+describe('channelsCreateV1', () =>{
   test('invalid authUserId', () => {
     const email = 'z5555555@ad.unsw.edu.au';
     const password = 'password';
@@ -169,4 +169,79 @@ describe('ChannelsCreateV1', () =>{
     const channel2 = channelsCreateV1(authUserId2, name, isPublic);
     expect(channel1).not.toStrictEqual(channel2);
   });
+});
+
+
+describe('channelDetailsV1 ', () => {
+  let email, password, nameFirst, nameLast, authUserObj;
+  beforeEach(() => {
+      email = 'z5555555@ad.unsw.edu.au';
+      password = 'password';
+      nameFirst = 'Snoopy';
+      nameLast = 'the Dog';
+      
+      authUserObj = authRegisterV1(email, password, nameFirst, nameLast);
+  });
+
+  test('authUserId is invalid', () => {
+      const channelObj = channelsCreateV1(authUserObj.authUserId,'COMP1531 Crunchie', false);
+      expect(channelDetailsV1(authUserObj.authUserId + 1)).toStrictEqual(ERROR)
+  });
+
+  test('channelId is invalid', () => {
+      const channelObj = channelsCreateV1(authUserObj.authUserId,'COMP1531 Crunchie', false);
+      expect(channelDetailsV1(authUserObj.authUserId, channelObj.channelId + 1)).toStrictEqual(ERROR)
+  });
+
+  test('valid authUserId but not a part of the channel', () => {
+      email = 'z5333333@ad.unsw.edu.au';
+      password = 'yellowfeathers';
+      nameFirst = 'Big';
+      nameLast = 'Bird';
+      
+      const authUserObj2 = authRegisterV1(email, password, nameFirst, nameLast);
+
+      const channelObj = channelsCreateV1(authUserObj2.authUserId,'COMP1531 Crunchie', false);
+      expect(channelDetailsV1(authUserObj.authUserId, channelObj.channelId)).toStrictEqual(ERROR)
+  });
+
+  test('valid authUserId is part of a channel', () => {
+      const channelObj = channelsCreateV1(authUserObj.authUserId,'COMP1531 Crunchie', false);
+
+      expect(channelDetailsV1(authUserObj.authUserId, channelObj.channelId)).toStrictEqual({
+          channels: [
+              {
+                  channelId: channelObj.channelId,
+                  isPublic: false,
+                  ownerMembersIds: [authUserObj.authUserId],
+                  allMembersIds: [authUserObj.authUserId],
+                  
+              },
+          ]
+      })
+  })
+
+  test('multiple valid authUserIds are a part of the channel', () => {
+      const email = 'z5555555@ad.unsw.edu.au';
+      const password = 'password';
+      const nameFirst = 'Charlie';
+      const nameLast = 'Brown';
+      const authUserObj2 = authRegisterV1(email, password, nameFirst, nameLast);
+  
+      const channelObj2 = channelsCreateV1(authUserObj2.authUserId,'COMP1531 Crunchie', false);
+
+      channelJoinV1(authUserObj.authUserId, channelObj.channelId);
+
+      expect(channelDetailsV1(authUserObj.authUserId, channelObj.channelId)).toStrictEqual({
+          channels: [
+              {
+                  channelId: channelObj.channelId,
+                  isPublic: false,
+                  ownerMembersIds: [authUserObj.authUserId],
+                  allMembersIds: [authUserObj.authUserId, authUserObj2.authUserId],
+                  
+              },
+          ]
+      })
+  })
 });
